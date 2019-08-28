d79a602a4a7e7323f30f7f42e03946e8af4e1d78
Ola Siem <info@siemieniuk.net>
2019-08-28 16:16:55 +0200

    Add 'Product' constructor and 1st instance

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,33 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(){
+      const thisProduct = this;
+
+      console.log('new product:', thisProduct);
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+      // const thisApp = this;
+      // console.log('thisApp.data: ', thisApp.data);
+      // const testProduct = new Product();
+      // console.log('testProduct: ', testProduct);
+    },
+
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +86,10 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+
+      thisApp.initMenu();
     },
   };
 
