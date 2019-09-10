cc7e820678c8be0814b88d40c158039f84ce1abc
Ola Siem <info@siemieniuk.net>
2019-09-10 16:39:30 +0200

    Add cart toggle

--- src/js/script.js
+++ src/js/script.js
@@ -336,6 +336,36 @@
   /* END class AmountWidget */
   }
 
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+      // console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = document.querySelector(select.cart.toggleTrigger);
+
+      console.log(Cart);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  /* END class Cart */
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -354,6 +384,12 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       // console.log('*** App starting ***');
@@ -365,6 +401,9 @@
       thisApp.initData();
 
       thisApp.initMenu();
+
+      thisApp.initCart();
+
     },
   };
 
