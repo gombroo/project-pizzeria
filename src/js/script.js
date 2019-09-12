ba8f2101fb06c3156acf9beeb8a1680a9662d28c
Ola Siem <info@siemieniuk.net>
2019-09-12 19:06:48 +0200

    Remove product from cart

--- src/js/script.js
+++ src/js/script.js
@@ -401,6 +401,10 @@
       thisCart.dom.productList.addEventListener('updated', function(){
         thisCart.update();
       });
+
+      thisCart.dom.productList.addEventListener('remove', function(){
+        thisCart.remove(event.detail.cartProduct);
+      });
     }
 
     add(menuProduct){
@@ -451,12 +455,22 @@
         }
       }
 
-      console.log('thisCart.totalNumber', thisCart.totalNumber);
-      console.log('thisCart.subtotalPrice', thisCart.subtotalPrice);
-      console.log('thisCart.totalPrice', thisCart.totalPrice);
-
+      // console.log('thisCart.totalNumber', thisCart.totalNumber);
+      // console.log('thisCart.subtotalPrice', thisCart.subtotalPrice);
+      // console.log('thisCart.totalPrice', thisCart.totalPrice);
     }
 
+    remove(cartProduct){
+      const thisCart = this;
+
+      const index = thisCart.products.indexOf(cartProduct);
+      const removedProduct = thisCart.products.splice(index, 1);
+
+      const removeDOM = cartProduct.dom.wrapper;
+      removeDOM.remove();
+
+      thisCart.update();
+    }
   /* END class Cart */
   }
 
@@ -473,6 +487,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
 
       //console.log('new thisCartProduct', thisCartProduct);
       //console.log('productData', menuProduct);
@@ -501,6 +516,32 @@
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });
     }
+
+    remove(){
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions(){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function(event){
+        event.preventDefault();
+      });
+
+      thisCartProduct.dom.remove.addEventListener('click', function(event){
+        event.preventDefault();
+        thisCartProduct.remove();
+      });
+    }
+
   /* END cart Product */
   }
 
